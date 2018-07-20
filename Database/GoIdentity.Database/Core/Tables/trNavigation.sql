51c46505bc81e01e48de608bd88862a94f747004
nm-a <nmreddy83@gmail.com>
2018-07-21 01:58:37 +0530

    script changes

---
+++ Database/GoIdentity.Database/Core/Tables/trNavigation.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [Core].[trNavigation]
+(
+	[NavigationId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	
+	[Title] [nvarchar](200) NOT NULL,
+	[NavigationPath] [varchar] (2000) NULL,
+	[LevelId] [TinyInt] NOT NULL,
+	[ParentNavigationId] [int] NULL,
+	[ImagePath] [varchar] (2000) NOT NULL,	
+	[SortId] [int] NOT NULL,
+	
+	[CreatedBy] INT NOT NULL,
+	[CreatedDate] DATETIME NOT NULL,
+	[ModifiedBy] INT NOT NULL,
+	[ModifiedDate] DATETIME NOT NULL
+
+)
+
+/*
+	Level 0 = Module
+	Level 1 = Sub Navigation Group
+	Level 2 = Direct Link
+*/
--- Database/GoIdentity.Database/Script.PostDeployment.sql
+++ Database/GoIdentity.Database/Script.PostDeployment.sql
@@ -14,3 +14,63 @@
 				0, NULL, 0, NULL, NEWID(),
 				0, GETDATE(), 0, GETDATE());
 	END
+
+
+	--Role
+	IF NOT EXISTS(SELECT 1 FROM Core.dmnRole)
+	BEGIN
+		INSERT INTO Core.dmnRole(Title,DefaultPage,IsActive,IsAdmin,
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('Admin', '', 1, 1,
+				0, GETDATE(), 0, GETDATE());
+
+		INSERT INTO Core.dmnRole(Title,DefaultPage,IsActive,IsAdmin,
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('Regular', '', 1, 0,
+				0, GETDATE(), 0, GETDATE());
+	END
+
+
+	declare @navigationId INT
+	--Navigation
+	IF NOT EXISTS(SELECT 1 FROM Core.trNavigation)
+	BEGIN
+		--Person
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('Core', '', 0, NULL, '', 1,
+				0, GETDATE(), 0, GETDATE());
+		select @navigationId = SCOPE_IDENTITY();
+
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('Member', '', 1, @navigationId, '', 1,
+				0, GETDATE(), 0, GETDATE());
+		select @navigationId = SCOPE_IDENTITY();
+
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('Dashboard', '/ops/dashboard', 2, @navigationId, '', 1,
+				0, GETDATE(), 0, GETDATE());
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('My Identity', '/ops/myidentity', 2, @navigationId, '', 2,
+				0, GETDATE(), 0, GETDATE());
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('My Profile', '/ops/myprofile', 2, @navigationId, '', 3,
+				0, GETDATE(), 0, GETDATE());
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('Notifications', '/ops/notifications', 2, @navigationId, '', 4,
+				0, GETDATE(), 0, GETDATE());
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('About', '/ops/about', 2, @navigationId, '', 5,
+				0, GETDATE(), 0, GETDATE());
+		INSERT INTO Core.trNavigation([Title], [NavigationPath], [LevelId], [ParentNavigationId], [ImagePath], [SortId],
+				[CreatedBy],[CreatedDate], [ModifiedBy], [ModifiedDate])
+		VALUES('Contact us', '/ops/contactus', 2, @navigationId, '', 6,
+				0, GETDATE(), 0, GETDATE());
+
+	END
