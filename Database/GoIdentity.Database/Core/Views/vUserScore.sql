651bbb104a4994b91910e181194ba2dbf7eb662d
nm-a <nmreddy83@gmail.com>
2018-07-21 16:04:33 +0530

    score calculation

--- Database/GoIdentity.Database/Core/Views/vUserScore.sql
+++ Database/GoIdentity.Database/Core/Views/vUserScore.sql
@@ -7,10 +7,9 @@ AS
 	group by UserId
 )
 select map.IndustryId, I.Name as Industry, I.Weightage as IndustryWeightage
-,map.CategoryId, C.Name as Category, TS.UserId, TS.Score
+,map.CategoryId, C.Name as Category, TS.UserId, cast(I.Weightage * TS.Score / 100 as decimal(18,2)) as Score
  from Core.trUserScore TS
 JOIN Core.dmnIndustryCategoryMap map on TS.ICMapId = map.Id
 JOIN Core.dmnIndustry I ON map.IndustryId = I.Id
 JOIN Core.dmnCategory C ON map.CategoryId = C.Id
 JOIN recentV R ON TS.UserId = R.UserId and TS.CreatedOn = R.MaxDate
-
