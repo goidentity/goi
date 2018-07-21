ca4ba76110985a5ce1b2d6a0b7cdf997be5ed0bc
nm-a <nmreddy83@gmail.com>
2018-07-21 15:58:11 +0530

    vUserScore

---
+++ Database/GoIdentity.Database/Core/Views/vUserScore.sql
@@ -0,0 +1,16 @@
+﻿create view Core.vUserScore
+AS
+with recentV
+AS
+(
+	select UserId, max(CreatedOn) as MaxDate from Core.trUserScore
+	group by UserId
+)
+select map.IndustryId, I.Name as Industry, I.Weightage as IndustryWeightage
+,map.CategoryId, C.Name as Category, TS.UserId, TS.Score
+ from Core.trUserScore TS
+JOIN Core.dmnIndustryCategoryMap map on TS.ICMapId = map.Id
+JOIN Core.dmnIndustry I ON map.IndustryId = I.Id
+JOIN Core.dmnCategory C ON map.CategoryId = C.Id
+JOIN recentV R ON TS.UserId = R.UserId and TS.CreatedOn = R.MaxDate
+
