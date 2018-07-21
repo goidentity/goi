b8e94ef1bdac0c91fc5fd86b17f71f5521e7ec2f
KranthiIdentity <kranthi@mail.com>
2018-07-21 20:15:14 +0000

    created, modified to int

--- Database/GoIdentity.Database/Core/Tables/trUserProfile.sql
+++ Database/GoIdentity.Database/Core/Tables/trUserProfile.sql
@@ -1,16 +1,16 @@
 ﻿CREATE TABLE [Core].[trUserProfile] (
-    [UserProfileId]   INT        IDENTITY (1, 1) NOT NULL,
-    [DOB]             NVARCHAR(50) NULL,
-    [Area]            NVARCHAR(50) NULL,
-    [Gender]          NVARCHAR(50) NULL,
-    [Profession]      NVARCHAR(50) NULL,
-    [RolesPlayed]     NVARCHAR(MAX) NULL,
-    [RolesInterested] NVARCHAR(MAX) NULL,
-    [UserId]          INT NOT NULL,
-    [CreatedBy] NVARCHAR(50) NULL, 
-    [CreatedDate] DATETIME NULL, 
-    [ModifiedBy] NVARCHAR(50) NULL, 
-    [ModifiedDate] DATETIME NULL, 
+    [UserProfileId]   INT            IDENTITY (1, 1) NOT NULL,
+    [DOB]             NVARCHAR (50)  NULL,
+    [Area]            NVARCHAR (50)  NULL,
+    [Gender]          NVARCHAR (50)  NULL,
+    [Profession]      NVARCHAR (50)  NULL,
+    [RolesPlayed]     NVARCHAR (MAX) NULL,
+    [RolesInterested] NVARCHAR (MAX) NULL,
+    [UserId]          INT            NOT NULL,
+    [CreatedBy]       INT  NULL,
+    [CreatedDate]     DATETIME       NULL,
+    [ModifiedBy]      INT  NULL,
+    [ModifiedDate]    DATETIME       NULL,
     PRIMARY KEY CLUSTERED ([UserProfileId] ASC)
 );
 
