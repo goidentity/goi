61aa8fad56421b90e8a6589481de56ab50e719dc
Mansoor Alikhan K <mansres@users.noreply.github.com>
2018-07-22 13:04:58 +0530

    Notification added

--- Web/GoIdentity.Web.App/src/app/ops/notifications.component.ts
+++ Web/GoIdentity.Web.App/src/app/ops/notifications.component.ts
@@ -1,38 +1,56 @@
 import { Component, OnInit, Inject, Input, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
-import { Subscription ,  Observable } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 
 import { ToasterService } from "angular2-toaster";
 import { AuthenticationService } from '../services/authentication.service';
+import { ScoreService } from '../services/score.service';
 import { Broadcaster, MessageEvent } from '../models/utilities/broadcaster';
 import { BaseComponent } from '../shared/base-component';
 
 @Component({
     templateUrl: './notifications.component.html',
-    providers: []
+    providers: [ScoreService, ModalModule]
 })
 export class NotificationsComponent extends BaseComponent implements OnInit {
     public router: Router;
     public OpenPanelDialogStatus: boolean;
-    public dataSetsGridView: Array<any>; 
+    public dataSetsGridView: Array<any>;
 
     constructor(
         public toasterService: ToasterService,
         public authenticationService: AuthenticationService,
         public messageEvent: MessageEvent,
-
+        public scoreService: ScoreService,
+        public modalModule: ModalModule,
         private routerObj: Router) {
         super(toasterService, authenticationService, messageEvent);
         this.router = routerObj;
     }
 
     ngOnInit() {
-        
-        this.getReportData();
+        this.getNotificationData();
     }
-    private getReportData(): void {
-        
+
+    public notificationData: any[];
+
+    private getNotificationData(): void {
+        this.scoreService.getNotifications(Number.parseInt(localStorage.getItem('UserId'))).subscribe(data => {
+            debugger
+            this.notificationData = data;
+        });
+    }
+
+    onDelete(userNotificationId: number) {
+        //this.modalModule.();
+        //console.log(userNotificationId)
+    }
+    onEdit(userNotificationId: number) {
+
+        //console.log(userNotificationId)
+
     }
     sourceList: Widget[] = [
         new Widget('onebyone'),
@@ -49,25 +67,25 @@ export class NotificationsComponent extends BaseComponent implements OnInit {
         this.targetList.push($event.dragData);
     }
     /* Init */
-    
+
 
     iproGridSettings() {
-      this.OpenPanelDialogStatus = true;
+        this.OpenPanelDialogStatus = true;
     }
 
 
     iproGridDelete(index: number) {
         debugger;
-        this.targetList.splice(index,1);        
-       
+        this.targetList.splice(index, 1);
+
     }
 
 
-     closePanel() {
-         this.OpenPanelDialogStatus = false;
-     }
+    closePanel() {
+        this.OpenPanelDialogStatus = false;
+    }
 
-    
+
 
 
 }
