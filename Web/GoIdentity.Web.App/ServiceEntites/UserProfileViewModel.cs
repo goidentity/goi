53d42063ec137dc7a878dee55a0166ce1ceb4982
KranthiIdentity <kranthi@goidentity.com>
2018-10-06 10:28:42 +0530

    CreateProfile changes

--- Web/GoIdentity.Web.App/ServiceEntites/UserProfileViewModel.cs
+++ Web/GoIdentity.Web.App/ServiceEntites/UserProfileViewModel.cs
@@ -7,10 +7,24 @@ namespace GoIdentity.Web.App.ServiceEntites
 {
     public class UserProfileViewModel
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string DisplayName { get; set; }
         public string DOB { get; set; }
+        public int MobileNumber { get; set; }
+        public int AlternateNumber { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
         public string Area { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
         public string Gender { get; set; }
         public string Profession { get; set; }
+        public string Company { get; set; }
         public string RolesPlayed { get; set; }
         public string RolesInterested { get; set; }
         public int UserId { get; set; }
--- Web/GoIdentity.Web.App/src/app/services/user.service.ts
+++ Web/GoIdentity.Web.App/src/app/services/user.service.ts
@@ -1,11 +1,11 @@
-import { catchError } from 'rxjs/operators';
+//import { catchError } from 'rxjs/operators';
 import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { catchError, map } from 'rxjs/operators';
 import { BaseService } from './base.service';
 import { AuthenticationService } from './authentication.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Navigation, UserProfile } from "../models/domain/user-entities";
 import { ChangePassword, Client } from '../models/domain/user-entities';
 
@@ -34,12 +34,42 @@ export class UserService extends BaseService {
         return this.authHttp.get<UserProfile>(this.baseServiceUrl + 'GetUserProfile/');
     }
 
-    createUserProfile(userProfileViewmodel: UserProfileViewmodel) {
-        return this.authHttp.post(this.baseServiceUrl + 'Save', JSON.stringify(userProfileViewmodel));
+    //createUserProfile(userProfileViewmodel: UserProfileViewmodel) {
+    //    return this.authHttp.post(this.baseServiceUrl + 'CreateUserProfile', JSON.stringify(userProfileViewmodel));
+    //}
+    public createUserProfile(firstname: string, lastname: string, middlename: string, displayname: string, dob: string, mobilenumber: string, alternatenumber: string, email: string, address1: string): Observable<any> {
+        let tokenEndpoint: string = "/api/UserApi/CreateUserProfile";
+        let model: any = {
+            FirstName: firstname,
+            LastName: lastname,
+            MiddleName: middlename,
+            DisplayName: displayname,
+            DOB: dob,
+            MobileNumber: mobilenumber,
+            AlternateNumber: alternatenumber,
+            Email: email,
+            Address1: address1,
+            Address2: firstname,
+            Area: lastname,
+            State: lastname,
+            City: lastname,
+            Zip: lastname,
+            Gender: lastname,
+            Profession: lastname,
+            Company: lastname,
+            RolesPlayed: lastname,
+            RolesInterested: lastname,
+            UserId: lastname
+        }
+        return this.authHttp.post<any>(tokenEndpoint, model).pipe(map(result => {
+            if (result != undefined) {
+                return result;
+            }
+        }));
     }
 
     updateUserProfile(userProfileViewmodel: UserProfileViewmodel) {
-        return this.authHttp.post(this.baseServiceUrl + 'Update', JSON.stringify(userProfileViewmodel));
+        return this.authHttp.post(this.baseServiceUrl + 'UpdateUserProfile', JSON.stringify(userProfileViewmodel));
     }
 
     forgotPassword(userName: string): Observable<boolean> {
@@ -58,10 +88,24 @@ export class RegisterViewModel
 
 export class UserProfileViewmodel
 {
+    FirstName: string;
+    LastName: string;
+    MiddleName: string;
+    DisplayName: string;
     DOB: string;
+    MobileNumber: string;
+    AlternateNumber: string;
+    Email: string;
+    Website: string;
+    Address1: string;
+    Address2: string;
     Area: string;
+    State: string;
+    City: string;
+    Zip: string;
     Gender: string;
     Profession: string;
+    Company: string;
     RolesPlayed: string;
     RolesInterested: string;
     UserId: string;
