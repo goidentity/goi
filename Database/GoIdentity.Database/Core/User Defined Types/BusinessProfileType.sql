d0abd9687a5c4bb14813dc8b06be2cb39d967d07
nm-a <nmreddy83@gmail.com>
2018-11-27 20:25:46 +0530

    added user id

--- Database/GoIdentity.Database/Core/User Defined Types/BusinessProfileType.sql
+++ Database/GoIdentity.Database/Core/User Defined Types/BusinessProfileType.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TYPE [Core].[BusinessProfileType] AS TABLE
 (
 	[BusinessProfileId] INT NULL,
+	UserId INT NULL,
+
 	CompanyName varchar(500) not null,
 	YearOfEstablishment smallint null,
 	ComponySize int,
