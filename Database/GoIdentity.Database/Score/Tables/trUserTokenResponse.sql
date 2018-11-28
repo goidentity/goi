8517ccdb7ae3d2918b26eac750034f180b633729
nm-a <nmreddy83@gmail.com>
2018-11-28 21:55:21 +0530

    db schema

---
+++ Database/GoIdentity.Database/Score/Tables/trUserTokenResponse.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Scores].[trUserTokenResponse]
+(
+	[UserTokenResponseId] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+	[UserId] INT NOT NULL, 
+
+	[Token] VARCHAR(8000) NOT NULL,
+	
+	[ProcessDate] DATETIME NULL,
+
+    [CreatedBy] INT NOT NULL,
+	[CreatedDate] DATETIME NOT NULL, 
+    [ModifiedBy] INT NOT NULL, 
+    [ModifiedDate] DATETIME NOT NULL
+)
---
+++ Database/GoIdentity.Database/Score/vUserTokenView.sql
@@ -0,0 +1,25 @@
+﻿CREATE VIEW [Core].[vUserTokenView]
+AS 
+	
+SELECT 1 as RowNo, U.UserId, 'Basic' as TokenName, U.FirstName + ' ' + U.LastName + ' ' + ISNULL(UPI.CityOfLiving, '') as Token FROM Core.trUser U
+LEFT JOIN Core.trUserPersonnelInfo UPI ON U.UserId = UPI.UserId
+
+UNION
+
+SELECT ROW_NUMBER() OVER (PARTITION BY U.UserId ORDER BY UPI.Title) as RowNo, U.UserId, 'Education' as TokenName, U.FirstName + ' ' + U.LastName + ISNULL(UPI.Title, '') as Token FROM Core.trUser U
+LEFT JOIN Core.trUserEducation UPI ON U.UserId = UPI.UserId and UPI.EducationType = 'Highest'
+
+UNION
+
+SELECT 1 as RowNo ,U.UserId, 'PrimaryIndustry' as TokenName, U.FirstName + ' ' + U.LastName + ISNULL(UPI.PrimaryIndustryOfWork, '') as Token FROM Core.trUser U
+LEFT JOIN Core.trUserPersonnelInfo UPI ON U.UserId = UPI.UserId
+
+UNION
+
+SELECT ROW_NUMBER() OVER (PARTITION BY U.UserId ORDER BY UPI.OrganizationName) as RowNo, U.UserId, 'Professional' as TokenName, U.FirstName + ' ' + U.LastName + ISNULL(UPI.OrganizationName, '') as Token FROM Core.trUser U
+LEFT JOIN Core.trUserExperience UPI ON U.UserId = UPI.UserId
+
+UNION
+
+SELECT ROW_NUMBER() OVER (PARTITION BY U.UserId ORDER BY UPI.CompanyName) as RowNo, U.UserId, 'Business' as TokenName, U.FirstName + ' ' + U.LastName + ISNULL(UPI.CompanyName, '') as Token FROM Core.trUser U
+LEFT JOIN Core.trBusinessProfile UPI ON U.UserId = UPI.UserId
